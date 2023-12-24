ffe3f2af6a00d7c23c69d8f5ebd363bcd7340a15
maxbond <ceo@rf.ru>
2023-12-25 00:25:39 +0300

    Compositions of Main Component

--- src/App.js
+++ src/App.js
@@ -56,7 +56,13 @@ export default function App() {
         <Search />
         <NumResults movies={movies} />
       </NavBar>
-      <Main movies={movies} />
+
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
@@ -95,16 +101,11 @@ function Search() {
   );
 }
 
-function Main({movies}) {
-  return (
-    <main className='main'>
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({children}) {
+  return <main className='main'>{children}</main>;
 }
 
-function ListBox({movies}) {
+function ListBox({children}) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -115,7 +116,7 @@ function ListBox({movies}) {
       >
         {isOpen1 ? '–' : '+'}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
