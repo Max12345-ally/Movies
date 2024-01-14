a1405f82142fb8fbe6ed74b10f181fb7439f4ce5
maxbond <ceo@rf.ru>
2024-01-14 23:26:39 +0300

    new starrating component added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom/client';
 
 import StarRating from './StarRating';
 
+function Test() {
+  return <StarRating color='blue' />;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,5 +18,6 @@ root.render(
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
     />
     <StarRating size={24} color='red' className='test' />
+    <Test />
   </React.StrictMode>
 );
