663fcae8105dcaef515f17afec02a5a9969426c6
maxbond <ceo@rf.ru>
2024-02-05 23:44:03 +0300

    error message component built

--- src/App.js
+++ src/App.js
@@ -105,6 +105,15 @@ function Loader() {
   return <p className='loader'>Loading...</p>;
 }
 
+function ErrorMessage({message}) {
+  return (
+    <p className='error'>
+      <span>📛</span>
+      {message}
+    </p>
+  );
+}
+
 function NavBar({children}) {
   return <nav className='nav-bar'>{children}</nav>;
 }
