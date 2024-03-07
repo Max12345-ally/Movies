5a2ceafa3a978a5e6a75c07863d6e4eed2070ce6
maxbond <ceo@rf.ru>
2024-03-07 23:39:16 +0300

    fetch added

--- src/App.js
+++ src/App.js
@@ -251,7 +251,14 @@ function Movie({movie, onSelectMovie}) {
 
 function MovieDetails({selectedId, onCloseMovie}) {
   useEffect(function () {
-    async function getMovieDetails() {}
+    async function getMovieDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+    getMovieDetails();
   }, []);
 
   return (
