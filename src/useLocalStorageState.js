ccbbfdfe984a25f134843105d63407d71ad6c13b
maxbond <ceo@rf.ru>
2024-05-22 23:43:04 +0300

    useLocalStorage extra useEffect

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -1,8 +1,15 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 export function useLocalStorageState(initialState) {
   const {value, setValue} = useState(function () {
     const storedValue = localStorage.getItem('watched');
     return JSON.parse(storedValue);
   });
+
+  useEffect(
+    function () {
+      localStorage.setItem('watched', JSON.stringify(value));
+    },
+    [value]
+  );
 }
